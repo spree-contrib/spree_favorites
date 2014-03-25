cde8a983e7287de4e21f17c66ea2539a489dfd9e
Jeff Dutil <jdutil@burlingtonwebapps.com>
2014-03-25 15:40:56 +0000

    More controller fixes.

--- app/controllers/spree/favorites_controller.rb
+++ app/controllers/spree/favorites_controller.rb
@@ -5,15 +5,17 @@ module Spree
     before_filter :find_favorite, :only => :destroy
 
     def index
-      @favorites = spree_current_user.favorite_products.page(params[:page]).per(Spree::Config.favorites_per_page)
+      @favorites = spree_current_user.favorites.page(params[:page]).per(Spree::Config.favorites_per_page)
     end
 
     def create
-      favorite = spree_current_user.favorites.new favorable_id: params[:favorable_id], favorable_type: params[:favorable_type]
-      if @success = favorite.save
+      @favorite = spree_current_user.favorites.new favorable_id: params[:favorable_id], favorable_type: params[:favorable_type]
+      if @success = @favorite.save
+        @favorable_id = @favorite.id
+        @favorable_type = @favorite.favorable_type
         @message = Spree.t(:successfully_added_favorite)
       else
-        @message = favorite.errors.full_messages.to_sentence
+        @message = @favorite.errors.full_messages.to_sentence
       end
       respond_to do |format|
         format.js
@@ -22,6 +24,8 @@ module Spree
 
     def destroy
       if @favorite
+        @favorable_id = @favorite.id
+        @favorable_type = @favorite.favorable_type
         @success = @favorite.destroy
       end
     end
