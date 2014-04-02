6f56593a3313b708f7bcad095efeefbf69da8e42
Jeff Dutil <jdutil@burlingtonwebapps.com>
2014-04-02 11:16:02 +0100

    Dont need hook run on fetch.

--- config/initializers/warden_after_auth_hook.rb
+++ config/initializers/warden_after_auth_hook.rb
@@ -1,5 +1,5 @@
 # Merges users favorites to their account after sign in and sign up.
-Warden::Manager.after_set_user do |user, auth, opts|
+Warden::Manager.after_set_user except: :fetch do |user, auth, opts|
   if auth.cookies.signed[:guest_token].present?
     Spree::Favorite.where(guest_token: auth.cookies.signed[:guest_token]).update_all(user_id: user.id, guest_token: nil)
   end
