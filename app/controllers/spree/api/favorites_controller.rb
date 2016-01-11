97d8d45eea6ccfd17d106f1d0653759ab3777df9
Kunal Chaudhari <kunalchaudhari@gmail.com>
2016-01-11 11:42:00 +0530

    ruby 1.9 syntax changes

--- app/controllers/spree/api/favorites_controller.rb
+++ app/controllers/spree/api/favorites_controller.rb
@@ -1,31 +1,35 @@
 module Spree
   module Api
     class FavoritesController < Spree::Api::BaseController
-
       def index
-        @favorites = @current_api_user.favorites.page(params[:page]).per(params[:per_page])
+        @favorites = @current_api_user.favorites.
+                                       page(params[:page]).
+                                       per(params[:per_page])
       end
 
       def create
-        @favorite = @current_api_user.favorites.new favorable_id: params[:product_id], favorable_type: "Spree::Product"
+        @favorite = @current_api_user.favorites.
+                                      new(favorable_id: params[:product_id],
+                                          favorable_type: "Spree::Product")
 
         @success = @favorite.save
         status = @success ? 200 : 422
 
-        render :status => status
+        render status: status
       end
 
       def unfavorite
-        @favorite = @current_api_user.favorites.find_by(favorable_id: params[:product_id], favorable_type: "Spree::Product")
+        @favorite = @current_api_user.favorites.
+                                      find_by(favorable_id: params[:product_id],
+                                              favorable_type: "Spree::Product")
 
         status = 422
         if @favorite
           @success = @favorite.destroy ? true : false
           status = @success ? 200 : 422
         end
-        render :status => status
+        render status: status
       end
-
     end
   end
 end
--- app/overrides/add_favorite_products_tab.rb
+++ app/overrides/add_favorite_products_tab.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
-  :virtual_path => 'spree/admin/shared/sub_menu/_product',
-  :name => 'add_favorite_products_tab',
-  :insert_bottom => "ul#sidebar-product",
-  :text => "<%= tab :favorites %>"
+  virtual_path: "spree/admin/shared/sub_menu/_product",
+  name: "add_favorite_products_tab",
+  insert_bottom: "ul#sidebar-product",
+  text: "<%= tab :favorites %>"
 )
